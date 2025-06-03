3451974a93af564819ed1644665edae57d622f85
MindsMirrors <michaeljtrujillo92@gmail.com>
2025-06-03 00:38:44 -0600

    worked on free to game api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let contrastToggle = false;
+let games;
 
 function openMenu() {
     document.body.classList += " menu--open"
@@ -17,3 +18,25 @@ function toggleContrast() {
         document.body.classList.remove("dark-theme");
     }
 }
+
+const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '0a43c950bdmsh126f1842ed54776p1fc0bcjsnefc1964260ce',
+		'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
+	}
+};
+
+// try {
+    async function getGames() {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+    }
+// } catch (error) {
+// 	console.error(error);
+// }
+
+getGames();
+
